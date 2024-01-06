85fb282ec03d302e673f549faf650911dfb8fc6c
Brent <bcen0001@student.monash.edu>
2024-01-06 16:37:02 +1100

    Small changes to formatting of the RecipesPanel composable.

--- app/src/main/java/com/brentcodes/recipesapplication/ui/screens/Screens.kt
+++ app/src/main/java/com/brentcodes/recipesapplication/ui/screens/Screens.kt
@@ -1,6 +1,7 @@
 package com.brentcodes.recipesapplication.ui.screens
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,13 +12,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -26,7 +31,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.brentcodes.recipesapplication.model.dataSpoonacular.Results
@@ -105,7 +112,10 @@ fun SearchBar(
     var text by remember {
         viewModel.query
     } 
-    Row {
+    Row(
+        modifier = Modifier
+            .padding(20.dp)
+    ) {
         TextField(
             value = text,
             maxLines = 1,
@@ -118,12 +128,19 @@ fun SearchBar(
             label = { Text("Search") },
             leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null)},
             modifier = modifier
-                .fillMaxWidth(0.9F)
+                .fillMaxWidth(0.8F)
         )
-        Button(
-            onClick = {onSearch(text)}
+        IconButton(
+            onClick = {onSearch(text)},
+            modifier = Modifier
+                .background(Color(0xFF00abe3), CircleShape)
+                .padding(20.dp, 0.dp, 0.dp, 0.dp)
         ) {
-            Text(text = "Search")
+            Icon(
+                imageVector = Icons.Rounded.Search,
+                contentDescription = null,
+                tint = Color.White
+            )
         }
     }
 
@@ -171,12 +188,19 @@ fun RecipesPanel(
             .fillMaxWidth()
             .height(100.dp)
             .padding(20.dp)
-            .border(2.dp, color = Color.Black)
+            .background(Color(0xFF00abe3))
+            .clip(RoundedCornerShape(5.dp))
+            .border(2.dp, Color(0xFF00abe3))
     ) {
         Text(
-            text = recipe.title?:"No title",
+            text = recipe.title?:"No title".split(" ").joinToString(separator = " ") {
+                it.replaceFirstChar(Char::titlecaseChar)
+            },
             modifier = Modifier
                 .align(Alignment.TopStart)
+                .padding(5.dp),
+            fontWeight = FontWeight.Bold,
+            color = Color.White
         )
         AsyncImage(
             model = recipe.image!!,
