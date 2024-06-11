3755b8081e20d14857cd650dedaf7e92f5af65fc
Brent <bcen0001@student.monash.edu>
2024-06-11 14:12:18 +1000

    Starting redesign of Recipe App

---
+++ app/src/main/java/com/brentcodes/recipesapplication/ui/screens/OnboardingScreens.kt
@@ -0,0 +1,18 @@
+package com.brentcodes.recipesapplication.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun OnboardingScreens(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    title: String,
+    subtitle: String,
+) {
+
+}
